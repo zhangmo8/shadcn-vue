c9b5dc14db091aaf67262f3272bdab502ceb34e2
zhangmo8 <wegi866@gmail.com>
2024-10-25 17:58:52 +0800

    feat(docs): highlight code block

--- apps/www/.vitepress/theme/config/shiki.ts
+++ apps/www/.vitepress/theme/config/shiki.ts
@@ -1,6 +1,41 @@
-import { createCssVariablesTheme } from 'shiki'
+import type { HighlighterCore } from 'shiki/core'
+import type { ThemeOptions } from 'vitepress'
+import { computedAsync } from '@vueuse/core'
+import { createHighlighterCore } from 'shiki/core'
+import { createJavaScriptRegexEngine } from 'shiki/engine/javascript'
 
-export const cssVariables = createCssVariablesTheme({
-  variablePrefix: '--shiki-',
-  variableDefaults: {},
+export const shikiThemes: ThemeOptions = {
+  light: 'vitesse-light',
+  dark: 'vitesse-dark',
+}
+
+export const highlighter = computedAsync<HighlighterCore>(async (onCancel) => {
+  const shiki = await createHighlighterCore({
+    engine: createJavaScriptRegexEngine(),
+    themes: [
+      () => import('shiki/themes/vitesse-dark.mjs'),
+      () => import('shiki/themes/vitesse-light.mjs'),
+    ],
+    langs: [
+      () => import('shiki/langs/javascript.mjs'),
+      () => import('shiki/langs/vue.mjs'),
+    ],
+  })
+
+  onCancel(() => shiki?.dispose())
+  return shiki
 })
+
+export function highlight(code: string, lang: string) {
+  if (!highlighter.value)
+    return code
+
+  return highlighter.value.codeToHtml(code, {
+    lang,
+    defaultColor: false,
+    themes: {
+      dark: 'vitesse-dark',
+      light: 'vitesse-light',
+    },
+  })
+}
